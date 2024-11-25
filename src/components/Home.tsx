68dc4092bc03ffe4eaf40be78e4938b37fe2509c
james.wang <james.wang@cloudysys.com>
2024-11-25 18:26:42 +0800

    delete .env, change Home text color

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="text-center">
+    <div className="text-center text-white">
       <h1 className="text-4xl font-bold mb-8">歡迎使用使用者管理系統</h1>
       <p className="mb-8">請從上方選單選擇要使用的功能</p>
     </div>
